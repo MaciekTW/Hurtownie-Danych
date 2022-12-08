d9f8e2777a7bacc2bd5451291f413b20939d2344
Maciek <twarogmaciej2000@gmail.com>
2022-12-08 11:24:08 +0100

    Projekt

---
+++ Projekt/SQLQuery1.sql
@@ -0,0 +1,61 @@
+use projektMTProd;
+
+select * from DIMDate;
+
+select
+	[date]
+	,avg(amount) over (order by [fDate] rows between 5 preceding and current row ) as Avg_amount
+from Fact_sales;
+
+
+
+select fMonth,
+	[113],[972],[360],[700],[556],[291]
+from 
+(
+select 
+      [customer_idkey]
+      ,[amount]
+	,month(fDate) as fMonth
+  from Fact_sales
+) as SourceTable
+pivot
+(
+ sum(amount)
+  for [customer_idkey] IN ([113],[972],[360],[700],[556],[291])
+) as PivotTable
+
+
+ with Sumamount as
+ (
+ select year(fDate) as fYear
+		,sum([Amount]) as SumAmount
+ from Fact_sales
+ group by year(fDate)
+ )
+SELECT 
+      fYear
+	  ,stdev(SumAmount) over(order by fYear desc) as Std
+  from Sumamount
+
+
+select carrier,
+	[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12] 
+from 
+(
+select 
+      month(fDate) as fMonth
+      ,[amount]
+	, carrier_name as carrier
+  from Fact_sales
+  join Carriers on Carriers.carrier_id = Fact_sales.carrier_idkey
+) as SourceTable
+pivot
+(
+	sum(amount)
+  for fMonth IN ([1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12])
+) as PivotTable
+
+
+
+
